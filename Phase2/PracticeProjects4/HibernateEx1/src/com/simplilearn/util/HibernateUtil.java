6b2f1eec7b7d68f48ad90e01ff825fa06f2e79a5
JayanDivya <97792772+JayanDivya@users.noreply.github.com>
2022-02-01 13:50:33 +0530

    Add files via upload

---
+++ Phase2/PracticeProjects4/HibernateEx1/src/com/simplilearn/util/HibernateUtil.java
@@ -0,0 +1,23 @@
+package com.simplilearn.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.simplilearn.entity.EProduct;
+
+public class HibernateUtil {
+
+	public static SessionFactory buildSessionFactory() {
+		
+		// Step 1
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		cfg.addAnnotatedClass(EProduct.class);
+		
+		//Step 2
+		SessionFactory sf = cfg.buildSessionFactory();
+
+		return sf;
+	}
+
+}
