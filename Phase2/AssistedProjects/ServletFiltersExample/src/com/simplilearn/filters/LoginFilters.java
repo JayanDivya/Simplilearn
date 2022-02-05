3f07d669b4c649030b946bd5769988332ea23c95
JayanDivya <97792772+JayanDivya@users.noreply.github.com>
2022-02-05 08:08:10 +0000

    Add files via upload

---
+++ Phase2/AssistedProjects/ServletFiltersExample/src/com/simplilearn/filters/LoginFilters.java
@@ -0,0 +1,60 @@
+package com.simplilearn.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class LoginFilters
+ */
+@WebFilter(urlPatterns= {"/dashboard","/profile"})
+public class LoginFilters implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilters() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("Filter code executed");
+		
+		String userId = request.getParameter("userId");
+		
+		if(userId !=null && userId.equals("john")) {
+			
+			System.out.println("Before processing servlet");
+			chain.doFilter(request, response);
+			System.out.println("After processing servlet");
+			
+			return;
+		}
+		
+		System.out.println("Block request");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
